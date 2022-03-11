fd73adccb7392a0f4b76cffda07593581284bc01
fred0chieng <fredrickochiengachieng@gmail.com>
2022-03-11 12:48:49 -0800

    Puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - This is the starting point
+ *
+ * Return: Programming is fun
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
