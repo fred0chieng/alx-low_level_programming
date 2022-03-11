2f90feded069611e9a299408372d0010de700582
fred0chieng <fredrickochiengachieng@gmail.com>
2022-03-11 13:14:49 -0800

    Lord

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - Point that acts as entry point
  *
- * Return: Zero always when successful compilation
+ * Return: Always 0 when it is a succesfull compilation
  */
 int main(void)
 {	printf("with proper grammar, but the outcome is a piece of art, \n");
