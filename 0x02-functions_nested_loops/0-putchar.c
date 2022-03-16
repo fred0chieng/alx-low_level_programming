ff86edb587ea86ff5c473e559c7dd5358e9fa1c2
fred0chieng <fredrickochiengachieng@gmail.com>
2022-03-16 08:38:12 -0700

    Char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - this is the entry point
+ *
+ *Return :Returns zero when success
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * C is a programming language
+ */
+
+_putchar(c);
+
